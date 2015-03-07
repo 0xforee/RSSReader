eabc695359cb5c89cddf41b2db44096b644cb842
foree <beijing.2008.lm@163.com>
2015-03-07 15:48:01 +0800

    加入文件操作的工具类，方便后边对缓存的操作

---
+++ app/src/main/java/com/utils/foree/rssreader/FileUtils.java
@@ -0,0 +1,46 @@
+package com.utils.foree.rssreader;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by foree on 3/7/15.
+ * 文件工具类：给定一个用地址初始化的file对象，就可以写入或读取文件
+ */
+public class FileUtils {
+    //读取文件
+    public String readFile(File file) throws IOException {
+        FileReader in = null;
+        BufferedReader bufferedReader = null;
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        try {
+            in = new FileReader(file);
+            bufferedReader = new BufferedReader(in);
+            while ((temp = bufferedReader.readLine()) != null) {
+                stringBuffer.append(temp).append("\r\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null)
+                bufferedReader.close();
+            if (in != null)
+                in.close();
+        }
+        return stringBuffer.toString();
+    }
+
+    //写入文件
+    public void writeFile(File file, String string) throws IOException {
+        FileWriter out = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(out);
+        bufferedWriter.write(string);
+        bufferedWriter.close();
+        out.close();
+    }
+}
