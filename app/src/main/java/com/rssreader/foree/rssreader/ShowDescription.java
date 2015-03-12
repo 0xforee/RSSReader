345d14ca1d76161c49226ae733b1dff76d66352e
foree <beijing.2008.lm@163.com>
2015-03-12 13:54:53 +0800

    增加文章的分享功能

--- app/src/main/java/com/rssreader/foree/rssreader/ShowDescription.java
+++ app/src/main/java/com/rssreader/foree/rssreader/ShowDescription.java
@@ -2,10 +2,13 @@ package com.rssreader.foree.rssreader;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,6 +24,8 @@ import com.baseapplication.foree.rssreader.BaseActivity;
  */
 public class ShowDescription extends BaseActivity {
     private static final String TAG = "ShowDescription";
+    String link = null;
+    String title = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,6 @@ public class ShowDescription extends BaseActivity {
 
         //接收数据
         String content = null;
-        String link = null;
         Intent intent = getIntent();
 
         if (intent != null) {
@@ -40,20 +44,10 @@ public class ShowDescription extends BaseActivity {
                     + mBundle.getString("pubdate") + "\n"
                     + mBundle.getString("link");
 
+
             link = mBundle.getString("link");
-
+            title = mBundle.getString("title");
         }
-/*
-        TextView textView = (TextView)findViewById(R.id.textview);
-        textView.setText(content);
-
-        Button button = (Button)findViewById(R.id.back);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        }); */
         WebView webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         //加载webview中的js脚本
@@ -61,4 +55,32 @@ public class ShowDescription extends BaseActivity {
         webView.loadUrl(link);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_description, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            //调用分享按钮分享
+            case R.id.action_share:
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TITLE, title);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, link);
+                shareIntent.setType("text/plain");
+                startActivity(shareIntent);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
