1f26f366414e77894f3802dacc5261b4922ba376
foree <beijing.2008.lm@163.com>
2015-03-16 19:48:15 +0800

    添加Settings设置界面的actionBar

--- app/src/main/java/com/rssreader/foree/rssreader/SettingsActivity.java
+++ app/src/main/java/com/rssreader/foree/rssreader/SettingsActivity.java
@@ -1,22 +1,27 @@
 package com.rssreader.foree.rssreader;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+
+import com.baseapplication.foree.rssreader.BaseActivity;
 
 /**
  * Created by foree on 3/11/15.
  * settings的activity
  */
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_activity);
 
-        // 使用fragment来替换主界面的activity
+        //添加ActionBar的标题
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(R.string.action_settings);
+        // 使用fragment来替换主界面的fragment
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.fr_settings, new SettingsFragment())
                 .commit();
-
     }
 }
