c338b2a357ad7550662418a520da161361234051
foree <beijing.2008.lm@163.com>
2015-03-16 20:33:15 +0800

    settingFragment改用内部类来实现

--- app/src/main/java/com/rssreader/foree/rssreader/SettingsActivity.java
+++ app/src/main/java/com/rssreader/foree/rssreader/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.rssreader.foree.rssreader;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
 
 import com.baseapplication.foree.rssreader.BaseActivity;
@@ -23,5 +24,16 @@ public class SettingsActivity extends BaseActivity {
         getFragmentManager().beginTransaction()
                 .replace(R.id.fr_settings, new SettingsFragment())
                 .commit();
+    }
+
+    //settingFragment的内部类实现
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // 从资源文件中加载一个配置项
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
