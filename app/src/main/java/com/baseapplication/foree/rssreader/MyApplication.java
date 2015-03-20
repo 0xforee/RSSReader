91f24ca249bd27aee2265a681edc5409111b079b
foree <beijing.2008.lm@163.com>
2015-03-20 11:16:43 +0800

    规范MyApplication的功能职责

--- app/src/main/java/com/baseapplication/foree/rssreader/MyApplication.java
+++ app/src/main/java/com/baseapplication/foree/rssreader/MyApplication.java
@@ -2,10 +2,8 @@ package com.baseapplication.foree.rssreader;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.nfc.Tag;
 import android.os.Environment;
 import android.util.Log;
 
@@ -16,9 +14,7 @@ import com.rssreader.foree.rssreader.NavigationDrawerFragment;
 import com.utils.foree.rssreader.NetworkUtils;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.Manifest;
 
 /**
  * Created by foree on 3/7/15.
@@ -40,6 +36,8 @@ public class MyApplication extends Application {
 
     //应用程序名称
     public static final String myApplicationName = "RSSReader";
+    //应用程序包名
+    public static final String myApplicationPackageName = "com.rssreader.foree.rssreader";
     //应用程序版本名称
     public static String myVersionName;
     //应用程序版本号
@@ -58,7 +56,6 @@ public class MyApplication extends Application {
     //初始化环境
     public void initEnv() {
         //判断当前网络状况
-        // NetworkUtils networkUtils = new NetworkUtils();
         mNetworkState = NetworkUtils.getNetworkState(mContext);
 
         //如果当前Sdcard已经挂载，应用程序目录与缓存目录是否建立完成
@@ -66,12 +63,16 @@ public class MyApplication extends Application {
             //应用程序目录
             File myDateDir = new File(SdcardPath + "/RSSReader/");
             if (!myDateDir.exists())
-                myDateDir.mkdir();
+                if (!myDateDir.mkdir()) {
+                    Log.e(TAG, "创建应用程序目录失败");
+                }
             mySdcardDataDir = SdcardPath + "/RSSReader";
             //缓存目录
             File myCacheDir = new File(mySdcardDataDir + "/" + "cache/");
             if (!myCacheDir.exists())
-                myCacheDir.mkdir();
+                if (!myCacheDir.mkdir()) {
+                    Log.e(TAG, "创建缓存目录失败");
+                }
             mySdcardCacheDir = mySdcardDataDir + "/" + "cache";
         }
 
@@ -80,45 +81,20 @@ public class MyApplication extends Application {
         List<RssFeedInfo> rssFeedInfoList = rssDao.findAll();
         for (RssFeedInfo rssFeedInfo : rssFeedInfoList) {
             NavigationDrawerFragment.FeedInfos.add(rssFeedInfo.getTitle());
-
         }
-        Log.v(TAG, "初始化数据库成功");
+        Log.v(TAG, "数据库初始化成功");
 
         //获取当前应用程序的版本号和版本名称
-        getApplicationVersionName(mContext);
+        initApplicationVersionInfo(mContext);
 
-        Log.v(TAG, "初始化环境变量成功");
+        Log.v(TAG, "环境变量初始化成功");
 
-    }
-
-   /* //初始化一些系统设置
-    public void initSettings(Context context) {
-        //获取系统设置文件
-        File file = new File(context.getFilesDir() + "/../shared_prefs" + "/settings.xml");
-        if (!file.exists()) {
-            SharedPreferences sp = context.getSharedPreferences("settings", MODE_PRIVATE);
-            SharedPreferences.Editor editor = sp.edit();
-            editor.putBoolean("darktheme", false);
-            editor.putBoolean("wificache", false);
-            editor.putBoolean("cleancache", false);
-            editor.apply();
-        } else
-            Log.v(TAG, "设置文件存在，不进行初始化");
-    }*/
-
-    //清除列表缓存文件
-    public void cleanListCache() {
-        File file = new File(MyApplication.mySdcardCacheDir + "/listcache");
-        if (file.exists()) {
-            if (file.delete())
-                Log.v(TAG, "cache cleand");
-        }
     }
 
     //获取应用程序的版本信息
-    public void getApplicationVersionName(Context context) {
+    public void initApplicationVersionInfo(Context context) {
         PackageManager packageManager = context.getPackageManager();
-        PackageInfo packageInfo = null;
+        PackageInfo packageInfo;
         try {
             //获取当前包的信息
             packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
