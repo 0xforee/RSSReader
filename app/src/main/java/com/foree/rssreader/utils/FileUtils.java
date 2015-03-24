bfc8614d8acb1e1bdce056adccc545ec8ea7bbd6
foree <beijing.2008.lm@163.com>
2015-03-24 21:23:53 +0800

    重写读取bitmap的方法(压缩图片至imageview大小)

--- app/src/main/java/com/foree/rssreader/utils/FileUtils.java
+++ app/src/main/java/com/foree/rssreader/utils/FileUtils.java
@@ -8,6 +8,8 @@ import com.foree.rssreader.base.MyApplication;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -70,7 +72,7 @@ public class FileUtils {
             if (file.createNewFile()) {
                 FileOutputStream output = new FileOutputStream(file);
                 //压缩bitmap
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, output);
                 output.flush();
                 output.close();
             }
@@ -83,6 +85,42 @@ public class FileUtils {
      * 从内存卡中获取bitmap文件
      */
     public static Bitmap getBitmap(String fileName) {
-        return BitmapFactory.decodeFile(MyApplication.mySdcardCacheDir + File.separator + fileName);
+        return decodeFile(new File(MyApplication.mySdcardCacheDir + File.separator + fileName));
+    }
+
+    //decodes image and scales it to reduce memory consumption
+    private static Bitmap decodeFile(File f) {
+        try {
+            //decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            FileInputStream stream1 = new FileInputStream(f);
+            BitmapFactory.decodeStream(stream1, null, o);
+            stream1.close();
+
+            //Find the correct scale value. It should be the power of 2.
+            final int REQUIRED_SIZE = 198;
+            int width_tmp = o.outWidth, height_tmp = o.outHeight;
+            int scale = 1;
+            while (true) {
+                if (width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE)
+                    break;
+                width_tmp /= 2;
+                height_tmp /= 2;
+                scale *= 2;
+            }
+
+            //decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            FileInputStream stream2 = new FileInputStream(f);
+            Bitmap bitmap = BitmapFactory.decodeStream(stream2, null, o2);
+            stream2.close();
+            return bitmap;
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
