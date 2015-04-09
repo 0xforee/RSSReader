d8c322d69804113d26210c780c299a3d0af7b012
foree <beijing.2008.lm@163.com>
2015-04-09 15:43:30 +0800

    加入对夜间模式切换的监听事件

--- app/src/main/java/com/foree/rssreader/ui/SettingsActivity.java
+++ app/src/main/java/com/foree/rssreader/ui/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.foree.rssreader.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
 
 import com.foree.rssreader.base.BaseActivity;
 import com.foree.rssreader.base.MyApplication;
@@ -13,7 +17,9 @@ import com.rssreader.foree.rssreader.R;
  * Created by foree on 3/11/15.
  * settings的activity
  */
-public class SettingsActivity extends BaseActivity {
+public class SettingsActivity extends BaseActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "SettingsActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,35 @@ public class SettingsActivity extends BaseActivity {
                 .commit();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //解除注册
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //注册sharePreference监听器
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        if (key.equals("lp_darktheme")) {
+            Intent intent = new Intent(SettingsActivity.this, SettingsActivity.class);
+
+            finish();
+            overridePendingTransition(0, 0);
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            // overridePendingTransition(R.anim.stay, R.anim.alphaout);
+            Log.v(TAG, "onSharedPreferenceChanged");
+        }
+    }
     //settingFragment的内部类实现
     public static class SettingsFragment extends PreferenceFragment {
         private static final String TAG = "SettingsFragment";
