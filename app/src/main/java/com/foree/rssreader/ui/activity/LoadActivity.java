d5efa407ae9a6c88fd18954dfb63238854560d5a
foree <beijing.2008.lm@163.com>
2015-04-17 09:39:43 +0800

    修复bug

--- app/src/main/java/com/foree/rssreader/ui/activity/LoadActivity.java
+++ app/src/main/java/com/foree/rssreader/ui/activity/LoadActivity.java
@@ -25,6 +25,7 @@ public class LoadActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         //设置主题颜色
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
@@ -51,57 +52,56 @@ public class LoadActivity extends Activity {
                     setTheme(R.style.GrayTheme);
                     break;
             }
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_load);
-
-            //获取窗口,设置全屏
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-            //应用启动初始化环境变量
-            myApplication = new MyApplication(this);
-            myApplication.initEnv();
-
-            //找到两种字体
-            AssetManager assetManager = getAssets();
-            Typeface tf_mvboli = Typeface.createFromAsset(assetManager, "fonts/mvboli.ttf");
-            Typeface tf_hobostd = Typeface.createFromAsset(assetManager, "fonts/HoboStd.otf");
-
-            //找到speak
-            tv_speak1 = (TextView) findViewById(R.id.load_tv_speak1);
-            tv_speak2 = (TextView) findViewById(R.id.load_tv_speak2);
-            tv_speak3 = (TextView) findViewById(R.id.load_tv_speak3);
-            tv_speak_author = (TextView) findViewById(R.id.load_tv_speak_author);
-            tv_speak1.setTypeface(tf_mvboli);
-            tv_speak2.setTypeface(tf_mvboli);
-            tv_speak3.setTypeface(tf_mvboli);
-            tv_speak_author.setTypeface(tf_mvboli);
-
-            //找到version,并设置字体
-            tv_version = (TextView) findViewById(R.id.load_tv_version);
-            tv_version.setText(MyApplication.myApplicationVersion);
-            tv_version.setTypeface(tf_hobostd);
-
-            //find app_name, and setTypeface
-            tv_app_name = (TextView) findViewById(R.id.load_tv_app_name);
-            tv_app_name.setTypeface(tf_hobostd);
-
-            Thread loadThread = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        synchronized (this) {
-                            wait(2000);
-                            startActivity(new Intent(LoadActivity.this, MainActivity.class));
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } finally {
-                        finish();
-                    }
-                }
-            };
-            loadThread.start();
         }
+
+        setContentView(R.layout.activity_load);
+
+        //获取窗口,设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //应用启动初始化环境变量
+        myApplication = new MyApplication(this);
+        myApplication.initEnv();
+
+        //找到两种字体
+        AssetManager assetManager = getAssets();
+        Typeface tf_mvboli = Typeface.createFromAsset(assetManager, "fonts/mvboli.ttf");
+        Typeface tf_hobostd = Typeface.createFromAsset(assetManager, "fonts/HoboStd.otf");
+
+        //找到speak
+        tv_speak1 = (TextView) findViewById(R.id.load_tv_speak1);
+        tv_speak2 = (TextView) findViewById(R.id.load_tv_speak2);
+        tv_speak3 = (TextView) findViewById(R.id.load_tv_speak3);
+        tv_speak_author = (TextView) findViewById(R.id.load_tv_speak_author);
+        tv_speak1.setTypeface(tf_mvboli);
+        tv_speak2.setTypeface(tf_mvboli);
+        tv_speak3.setTypeface(tf_mvboli);
+        tv_speak_author.setTypeface(tf_mvboli);
+
+        //找到version,并设置字体
+        tv_version = (TextView) findViewById(R.id.load_tv_version);
+        tv_version.setText(MyApplication.myApplicationVersion);
+        tv_version.setTypeface(tf_hobostd);
+
+        //find app_name, and setTypeface
+        tv_app_name = (TextView) findViewById(R.id.load_tv_app_name);
+        tv_app_name.setTypeface(tf_hobostd);
+
+        Thread loadThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(2000);
+                        startActivity(new Intent(LoadActivity.this, MainActivity.class));
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        loadThread.start();
     }
 }
