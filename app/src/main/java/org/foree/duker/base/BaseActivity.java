cf37c6d7eaa6ae909b213145b2b461ba51ba6621
foree <beijing.2008.lm@163.com>
2016-07-18 21:46:09 +0800

    [CodeRefactoring]:增加切换主题开源库:AndroidChangeSkin

---
+++ app/src/main/java/org/foree/duker/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package org.foree.duker.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by foree on 16-7-18.
+ */
+public class BaseActivity extends AppCompatActivity{
+}
---
+++ app/src/main/java/org/foree/duker/ui/activity/SplashActivity.java
@@ -0,0 +1,9 @@
+package org.foree.duker.ui.activity;
+
+import org.foree.duker.base.BaseActivity;
+
+/**
+ * Created by foree on 16-7-18.
+ */
+public class SplashActivity extends BaseActivity{
+}
